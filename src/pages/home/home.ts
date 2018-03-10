730e509be5ffabb3ee7acd16ae1fe9b917dbe146
kelman <kelman.celis.iunigo.com>
2018-03-10 01:12:33 -0300

    some changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { UsersPage } from '../users/users';
-import { EmployeesListPage } from '../employees-list/employees-list';
-import { AngularFireAuth } from 'angularfire2/auth';
-import { ToastController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {UsersPage} from '../users/users';
+import {EmployeesListPage} from '../employees-list/employees-list';
+import {AngularFireAuth} from 'angularfire2/auth';
+import {ToastController} from 'ionic-angular';
 
 interface User {
   email: string;
@@ -16,36 +16,36 @@ interface User {
 })
 
 
-
 export class HomePage {
 
-  constructor(private afAuth: AngularFireAuth,public navCtrl: NavController, private toastCtrl: ToastController) {
-    
+  constructor(private afAuth: AngularFireAuth, public navCtrl: NavController, private toastCtrl: ToastController) {
+
   }
+
   user = {} as User;
 
-  openUsers(){
+  openUsers() {
 
-this.navCtrl.push (EmployeesListPage);
-
+    this.navCtrl.push(EmployeesListPage);
 
 
   }
-  async login(user: User) {
-    try {
-      const result = await this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password);
-      if (result) {
-        this.navCtrl.push (EmployeesListPage);
-      }  
-    }
-    catch (e) {
+
+  login(user: User) {
+
+    this.afAuth.auth.signInWithEmailAndPassword(user.email, user.password)
+      .then(() => {
+        this.navCtrl.push(EmployeesListPage);
+      }).catch((e) => {
       console.error(e);
-    }
+    })
+
+
   }
 
-  async register(user: User) {
+  register(user: User) {
     try {
-      const result = await this.afAuth.auth.createUserWithEmailAndPassword(
+      const result = this.afAuth.auth.createUserWithEmailAndPassword(
         user.email,
         user.password
       );
@@ -55,17 +55,17 @@ this.navCtrl.push (EmployeesListPage);
           duration: 3000,
           position: 'top'
         });
-      
+
         toast.onDidDismiss(() => {
           console.log('Dismissed toast');
         });
-      
+
         toast.present();
       }
     } catch (e) {
       console.error(e);
     }
   }
-  
+
 
 }
